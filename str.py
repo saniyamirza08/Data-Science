31596b17a211628c5dca28a481e9170e36247682
saniyamirza08 <saniya230803@gmail.com>
2025-01-10 18:44:57 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,180 @@
+#name="saniya"
+#n='saniya'
+#m="""saniya"""
+
+#rupees = 2000
+#print("I have ",rupees,"rupees")
+
+#y=21
+#print("I am ",y,"years old")
+
+#string functions
+
+#x="saniya mirza"
+#print(x.upper())
+#print(x.lower())
+
+#y="earth revolves around the sun"
+#print(y.capitalize())
+
+#z =  "     earth is elliptical shape      "
+#print(z.strip())
+#print(z.rstrip())
+#print(z.lstrip())
+
+#a="data scientist"
+#print(a.swapcase())
+
+#b="DATA ANALYST"
+#print(b.swapcase())
+
+#x="data mining"
+#print(x.title())
+
+#x="data stream mining"
+#print(x.replace("data","Data"))
+#print(x.find("stream"))
+#print(x.count("a"))
+
+
+#x="my name is saniya"
+#print(x.split(" "))
+
+#s="-"
+#print(s.join("name"))
+
+#a="data mining is the process of extracting knowledge"
+#print(a.startswith("mining"))
+#print(a.endswith("extracting"))
+
+#a="data mining is the process of extracting knowledge"
+#print(a.startswith("data"))
+#print(a.endswith("knowledge"))
+
+#x= "23082003"
+#print(x.isdigit())
+
+#c="abc"
+#print(c.isalpha())
+
+#q="saniya23"
+#print(q.isnumeric())
+#print(q.isalnum())
+
+#v="230803"
+#print(v.zfill(10))
+
+#s="saniya"
+#t="mirza"
+#u=s+t
+#print(u)
+
+#s="10"
+#t="13"
+#u=s+t
+#print(u)
+
+#age=23
+#text= f"i am {age}"
+#print(text)
+
+#x="saniya"
+#age=21
+#print("x : {} , age : {}".format(x,age))
+
+#x="data stream mining"
+#print(x.find("a"))
+
+#b="goal of data mining is to discover hidden patterns"
+#print(len(b))
+
+#a=4567
+#print(type(a))
+#print(type("saniya"))
+
+#a=9876
+#b=str(a)
+#print(type(b))
+
+#s="HELLO"
+#print(s.casefold())
+
+#s=" "
+#print(s.isspace())
+
+#s="sanu"
+#print(s.isspace())
+
+#s="Hello World"
+#print(s.istitle())
+
+#s="hello"
+#print(s.find("e"))
+
+#print("hello".find("e"))
+
+#print("hello".rfind("l"))
+
+#print("machine learning".index("e"))
+
+#print("machine learning".rindex("n"))
+
+#print('hello\nworld')
+
+#print("hello\nworld".splitlines())
+
+#print("data".ljust(8,"a"))
+#print("stream".rjust(7,"b"))
+#print("mining".center(6,"c"))
+
+
+#print("hello".ljust(5,"@"))
+#print("hello".rjust(90,"m"))
+#print("hello".center(80,"n"))
+
+#print("hello".ljust(10,"@"))
+
+#print("data".ljust(5,"@"))
+
+#print("hello".encode('utf-8'))
+
+#print(b'hello'.decode('utf-8'))
+
+
+#a="12ep"
+#print(a.isascii())
+
+#text="data science"
+#result = text.removesuffix("science")
+#print(result)
+
+#text="data stream mining"
+#esult = text.removeprefix("data")
+#print(result)
+
+#text="apple,banana,mango"
+#result=text.partition(",")
+#print(result)
+
+#s="hello\tworld"
+#print(s.expandtabs(tabsize=90))
+
+#table=str.maketrans("aeiou","12345")
+#text="hello world"
+#translated_text=text.translate(table)
+#print(translated_text)
+
+#s="data science"
+#print(s.find("stream"))
+
+a=input("enter a password")
+print(a)
+
+
+
+
+
+
+
+
+
